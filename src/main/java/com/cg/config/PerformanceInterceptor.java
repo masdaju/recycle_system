05e5b4266d6ae741379403ebb371d8fa2b0a6a59
masdaju <109020053+masdaju@users.noreply.github.com>
2025-04-10 20:27:55 +0800

    更新

--- src/main/java/com/cg/config/PerformanceInterceptor.java
+++ src/main/java/com/cg/config/PerformanceInterceptor.java
@@ -1,20 +1,20 @@
 package com.cg.config;
 
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
-
+@Slf4j
 public class PerformanceInterceptor implements InnerInterceptor {
-    private static final Logger logger = LoggerFactory.getLogger(PerformanceInterceptor.class);
+
 
     @Override
+    // 查询拦截
     public boolean willDoQuery(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
         long startTime = System.currentTimeMillis();
         try {
@@ -23,11 +23,12 @@ public class PerformanceInterceptor implements InnerInterceptor {
             long endTime = System.currentTimeMillis();
             long sqlCost = endTime - startTime;
             String sql = boundSql.getSql();
-            logger.info("Query SQL: {} 执行耗时: {} ms", sql, sqlCost);
+            log.info("Query SQL: {} 执行耗时: {} ms", sql, sqlCost);
         }
     }
 
     @Override
+    //增删改拦截
     public boolean willDoUpdate(Executor executor, MappedStatement ms, Object parameter) throws SQLException {
         long startTime = System.currentTimeMillis();
         BoundSql boundSql = ms.getBoundSql(parameter);
@@ -37,7 +38,8 @@ public class PerformanceInterceptor implements InnerInterceptor {
             long endTime = System.currentTimeMillis();
             long sqlCost = endTime - startTime;
             String sql = boundSql.getSql();
-            logger.info("Update SQL: {} 执行耗时: {} ms", sql, sqlCost);
+            log.info("Update SQL: {} 执行耗时: {} ms", sql, sqlCost);
         }
     }
+
 }    
